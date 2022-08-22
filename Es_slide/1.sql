047577d1a69a1b114f27ddc0396aef8119f41580
Filippo <pippodelministro@gmail.com>
2022-08-22 17:32:44 +0200

    es_fondo 3

---
+++ Es_slide/1.sql
@@ -0,0 +1,21 @@
+--*==================================================================================
+--*									ES SLIDE											
+--*==================================================================================
+
+--?Indicare l’incasso totale degli ultimi due anni, realizzato grazie alle visite dei
+--?medici cardiologi della clinica.
+
+
+--?Indicare il numero di pazienti di sesso femminile che, nel quindicesimo anno
+--?d’età, sono stati visitati, una o più volte, sempre dallo stesso ginecologo.
+
+
+--?Indicare codice fiscale, nome e cognome ed età del paziente più anziano della
+--?clinica, e il numero di medici dai quali è stato visitato.
+
+
+--?Indicare nome e cognome dei pazienti che sono stati visitati non meno di due
+--?volte dalla dottoressa Gialli Rita.
+
+
+--?Dato il paziente piu vecchio rovare quante volte è stato visitato da ogni medic
