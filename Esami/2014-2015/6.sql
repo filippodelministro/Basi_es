a6bd1d7b044ac264d8bd59fb3313b1b5055373ae
Filippo <pippodelministro@gmail.com>
2022-09-11 12:08:42 +0200

    Settembre-10_1

--- Esami/2014-2015/6.sql
+++ Esami/2014-2015/6.sql
@@ -1,13 +1,40 @@
-Esercizio 1 (7 punti)
-Scrivere una query che restituisca la dose giornaliera media dei farmaci indicati per la cura di sole patolo-
-gie intestinali.
-Esercizio 2 (7 punti)
-Scrivere una query che restituisca, per il sesso maschile e per quello femminile, rispettivamente, il numero
-di pazienti attualmente affetti da ipertensione, trattata con lo stesso farmaco da più di venti anni.
-Esercizio 3 (8 punti)
-Scrivere una query che, considerate le sole patologie muscolari, elimini gli esordi conclusi con guarigione
-relativi a pazienti che hanno contratto, e curato con successo, almeno due di tali patologie.
-Esercizio 4 (11 punti)
+--? Scrivere una query che restituisca la dose giornaliera media dei farmaci
+--? indicati per la cura di sole patologie intestinali.
+select I1.Farmaco, avg(I1.DoseGiornaliera) as Media
+from Indicazione I1
+where I1.Farmaco not in (
+		select I.Farmaco
+		from Indicazione I inner join Patologia P on I.Patologia = P.Nome
+		where P.ParteCorpo <> 'Intestino'
+)
+group by I1.Farmaco
+
+
+--? Scrivere una query che restituisca, per il sesso maschile e per quello
+--? femminile, rispettivamente, il numero di pazienti attualmente affetti da
+--? ipertensione, trattata con lo stesso farmaco da più di venti anni.
+select P.Sesso, count(*) as NumPaz
+from Paziente P inner join (
+		select T.Paziente
+		from Terapia T inner join Paziente P on T.Paziente = P.CodFiscale
+		where T.Patologia = 'Ipertensione'
+			and T.DataFineTerapia is null
+			and T.DataInizioTerapia + interval 20 year <= current_date()
+		group by T.Paziente
+		having count(distinct T.Farmaco) = 1
+) as D on P.CodFiscale = D.Paziente
+group by P.Sesso
+
+
+--? Scrivere una query che, considerate le sole patologie muscolari, elimini gli
+--? esordi conclusi con guarigione relativi a pazienti che hanno contratto, e
+--? curato con successo, almeno due di tali patologie.
+
+--? Eliminare esordi di sole patologie muscolari, che hanno una guarigione, di
+--? pazienti che hanno già guarito due volte tali patologie
+
+
+
 Negli ultimi mesi, la direzione della clinica è interessata al fenomeno della resistenza alle terapie per la pa-
 tologia influenzale. I pazienti target sono gli anziani aventi più di ottanta anni, affetti da almeno due pato-
 logie croniche. Dato un paziente target, interessano i suoi esordi di influenza degli ultimi tre anni. Conside-
