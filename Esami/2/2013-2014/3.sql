7275072d9df61efdfbf7b41e457bc3f8d73f8918
Filippo <pippodelministro@gmail.com>
2022-09-30 10:49:01 +0200

    Settembre-30_2

--- Esami/2/2013-2014/3.sql
+++ Esami/2/2013-2014/3.sql
@@ -5,8 +5,29 @@ from Farmaco F
 group by F.PrincipioAttivo
 
 --? Indicare nome e cognome dei pazienti che hanno contratto almeno due volte 
---? tutte le patologie intestinali.
+--? tutte le patologie cuore.
+with
+PatCuore as (
+		select Nome 
+		from Patologia 
+		where ParteCorpo = 'Cuore'
+)
 
+select P.Nome, P.Cognome
+from Esordio E inner join PatCuore PC on E.Patologia = PC.Nome
+			   inner join Paziente P on E.Paziente = P.CodFiscale
+where exists (
+	select *
+    from Esordio E1
+    where E1.Paziente = E.Paziente
+		and E1.Patologia = E.Patologia
+        and E1.DataEsordio <> E.DataEsordio
+)
+group by E.Paziente
+having count(distinct E.Patologia) = (
+	select count(*)
+    from PatCuore
+)
 
 
 --? Indicare le patologie esordite esclusivamente in forma cronica, curate 
@@ -25,6 +46,9 @@ where E.Patologia not in (
 --? un esordio, elimini l’esordio stesso, e ne inserisca un altro caratterizzato
 --? dalla nuova gravità, dalla data dell’aggiornamento come data di esordio, 
 --? e dagli stessi valori dell’esordio eliminato per i restanti attributi.
+
+
+
 
 --? Indicare la specializzazione medica che, considerate le visite effettuate
 --? dai suoi medici dall’anno 2010 a oggi, ha totalizzato ogni anno un numero
