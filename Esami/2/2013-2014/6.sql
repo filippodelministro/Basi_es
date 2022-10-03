e53fbcacb29cd94eb85947618defc7b21fe8f425
Filippo <pippodelministro@gmail.com>
2022-10-03 18:20:16 +0200

    Ottobre-03_2

--- Esami/2/2013-2014/6.sql
+++ Esami/2/2013-2014/6.sql
@@ -1,82 +1,23 @@
 --? Indicare nome e cognome di ciascun medico che ha visitato tutti i pazienti
 --? della sua città.
-select M1.Nome, M1.Cognome
-from Medico M1 inner join(
-		select V.Medico, P.Citta
-		from Paziente P inner join Visita V on P.CodFiscale = V.Paziente
-						inner join Medico M on M.Matricola = V.Medico
-		where M.Citta = P.Citta
-		group by V.Medico, P.Citta
-		having count(distinct V.Paziente) = (
-				select count(*)
-				from Paziente P1
-				where P1.Citta = P.Citta
-		)
-) as D on D.Medico = M1.Matricola
+
                 
 
 --? Indicare nome e cognome dei pazienti che hanno avuto, anche solo per un 
 --? giorno, più terapie in corso contemporaneamente.
 
-select distinct P.Nome, P.Cognome
-from Terapia T1 inner join Terapia T2 on (	-- cond. di Terapie diverse per un paz
-					T1.Paziente = T2.Paziente
-                    and T1.Patologia <> T2.Patologia
-                    and T1.Farmaco <> T2.Farmaco
-					and T1.DataInizioTerapia <> T2.DataInizioTerapia
-)
-				inner join Paziente P on T1.Paziente = P.CodFiscale
-where (	-- T1 e T2 terminate: controllo le date, entrambi i casi sono validi
-		T1.DataFineTerapia is not null
-		and(T2.DataFineTerapia is not null
-		and (T2.DataInizioTerapia between T1.DataInizioTerapia and T1.DataFineTerapia
-				or
-				T2.DataFineTerapia between T1.DataInizioTerapia and T1.DataFineTerapia
-            )
-		)
-)
-OR	-- T1 in corso, T2 terminata
-(T1.DataFineTerapia is null
-		and (T2.DataFineTerapia is not null
-		and (T2.DataInizioTerapia >= T1.DataInizioTerapia
-				or
-				T2.DataFineTerapia >= T1.DataInizioTerapia
-			)
-	)
-)
-OR	-- T1 terminata, T2 in corso
-	(T1.DataFineTerapia is not null
-		and T2.DataFineTerapia is null
-		and T2.DataInizioTerapia <= T1.DataFineTerapia
-		)
-OR
-	(	-- entrambe Terapie in corso
-        T1.DataFineTerapia is null and T2.DataFineTerapia is null
-);
 
 
 --? Indicare il reddito massimo fra quelli di tutti i pazienti che, nell’
 --? anno 2011, hanno effettuato esattamente tre visite, ognuna delle quali 
 --? con un medico avente specializzazione diversa dagli altri.
-select max(D.Reddito) as RedditoMax
-from (
-		select V.Paziente, P.Reddito
-		from Visita V inner join Medico M on V.Medico = M.Matricola
-					  inner join Paziente P on P.CodFiscale = V.Paziente
-		where year(V.Data) = 2011
-		group by V.Paziente
-		having count(*) = 3
-			and count(distinct M.Specializzazione) = 3
-) as D
 
 
---todo: ======================================================================
 --? Creare un vincolo di integrità generico (mediante un trigger) per impedire 
 --? che un medico possa visitare mensilmente più di due volte lo stesso 
 --? paziente, qualora all’atto delle due visite già effettuate in un dato 
 --? mese dal medico sul paziente, quest’ultimo non fosse affetto da alcuna 
 --? patologia.
---todo: ======================================================================
 
 
 --? Considerato ciascun farmaco per la cura di patologie gastroenterologiche,
@@ -85,35 +26,3 @@ from (
 --? ultimi cento anni, mantenere nella tabella INDICAZIONE la sola indicazione
 --? del farmaco considerato riguardante tale unica patologia, eliminando 
 --? tutte le altre.
-with
-UtilizziTot as (
-		select T.Farmaco, count(*) as NumUtilizzi
-		from Terapia T
-		where T.DataInizioTerapia > current_date() - interval 100 year
-			and T.Farmaco in (	-- Farmaci indicati per più patologie
-				select I.Farmaco
-				from Indicazione I inner join Patologia P on I.Patologia = P.Nome
-				where P.SettoreMedico = 'Gastroenterologia'
-				group by I.Farmaco	
-				having count(distinct I.Patologia) > 1	
-			)
-		group by T.Farmaco
-),
-UtilizziPat as (
-		select T.Farmaco, T.Patologia, count(*) as NumUtilizzi
-		from Terapia T
-		where T.DataInizioTerapia > current_date() - interval 100 year
-			and T.Farmaco in (	-- Farmaci indicati per più patologie
-				select I.Farmaco
-				from Indicazione I inner join Patologia P on I.Patologia = P.Nome
-				where P.SettoreMedico = 'Gastroenterologia'
-				group by I.Farmaco	
-				having count(distinct I.Patologia) > 1	
-			)
-		group by T.Farmaco, T.Patologia
-)
-
-select I.*
-from UtilizziTot UT inner join UtilizziPat UP on UT.Farmaco = UP.Farmaco
-					inner join Indicazione I on UP.Patologia = I.Patologia
-where UP.NumUtilizzi >= 0.6 * UT.NumUtilizzi
--- Esami/2/2013-2014/7.sql
+++ Esami/2/2013-2014/7.sql
@@ -1,18 +1,13 @@
 --? Considerato ogni principio attivo, indicarne il nome e il numero medio 
 --? di giorni per cui sono indicati i farmaci che lo contengono.
-select F.PrincipioAttivo, avg(I.NumGiorni) as Media
-from Farmaco F inner join Indicazione I on F.NomeCommerciale = I.Farmaco
-group by F.Principioattivo
 
 
 --? Indicare nome e cognome dei pazienti che, per curare gli esordi di almeno
 --? una patologia, hanno complessivamente assunto tutti i farmaci assunti
 --? da almeno un paziente per curare tale patologia 
 
---todo: ======================================================================
 --? Scrivere un evento che sconti mensilmente del 2% i farmaci che sono stati 
 --? assunti in meno del 10% delle terapie iniziate nel mese precedente.
---todo: ======================================================================
 
 
 --? In relazione a ciascuna patologia a carico dell’orecchio, indicarne il 
@@ -20,27 +15,3 @@ group by F.Principioattivo
 --? anno 2013, dai soli pazienti di Pisa e Siena, usando farmaci indicati
 --? unicamente per la patologia considerata, e con quale farmaco tale terapia
 --? è stata effettuata.
-with 
-FarmaciTarget as (
-		select P.Nome as Patologia, I.Farmaco
-		from Indicazione I inner join Patologia P on I.Patologia = P.Nome
-		where P.ParteCorpo = 'Orecchio'
-		group by P.Nome, I.Farmaco
-		having count(distinct I.Patologia) = 1
-),
-TerapieTarget as (
-		select T.Patologia, T.Farmaco, T.Posologia * datediff(T.DataFineTerapia, T.DataInizioTerapia) as NumCompresse
-		from FarmaciTarget FT inner join Terapia T on FT.Patologia = T.Patologia
-							  inner join Paziente P on T.Paziente = P.CodFiscale
-		where year(T.DataInizioTerapia) = 2013 and year(T.DataFineTerapia) = 2013
-			and (P.Citta = 'Pisa' or P.Citta = 'Siena')
-)
-
-select D.Patologia, min(D.PrezzoTerapia) as PrezzoMin
-from (
-		select TT.Patologia, TT.Farmaco, ((TT.NumCompresse % F.Pezzi)+1 * F.Costo) as PrezzoTerapia
-		from TerapieTarget TT inner join Farmaco F on TT.Farmaco = F.NomeCommerciale
-							  right outer join Patologia P on TT.Patologia = P.Nome
-		where P.ParteCorpo = 'Orecchio'
-) as D
-group by D.Patologia
