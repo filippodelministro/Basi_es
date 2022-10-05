5f983c98d90788d3250e43a4cd122ab868b36143
Filippo <pippodelministro@gmail.com>
2022-10-05 12:18:02 +0200

    Ottobre-05_3

--- Esami/2/2014-2015/1.sql
+++ Esami/2/2014-2015/1.sql
@@ -1,34 +1,12 @@
 --? Scrivere una query che restituisca il nome commerciale di ciascun 
 --? farmaco utilizzato da almeno un paziente per curare tutte le patologie
 --? per le quali è indicato.
-select distinct T.Farmaco
-from Terapia T
-group by T.Farmaco
-having count(distinct T.Patologia) = (
-		select count(distinct I.Patologia)
-        from Indicazione I
-        where I.Farmaco = T.Farmaco
-)
+
 
 --? Scrivere una query che restituisca il numero di pazienti visitati solo
 --? da medici specializzati in cardiologia o neurologia, almeno due volte 
 --? per ciascuna delle due specializzazioni. Si scriva la query senza usare viste.
-select count(distinct D.Paziente) as NumPazienti
-from (
-		select M1.Specializzazione, V1.Paziente
-		from Visita V1 inner join Medico M1 on V1.Medico = M1.Matricola
-		where V1.Paziente not in (
-				select V.Paziente
-				from Visita V inner join Medico M on V.Medico = M.Matricola
-				where M.Specializzazione <> 'Cardiologia'
-					and M.Specializzazione <> 'Neurologia'
-		)
-			and M1.Specializzazione = 'Cardiologia' or M1.Specializzazione = 'Neurologia'
-		group by M1.Specializzazione, V1.Paziente
-		having count(distinct V1.Data) > 1
-) as D
 
---todo: ======================================================================
 --? Creare una business rule che permetta di inserire un nuovo farmaco F e le
 --? relative indicazioni, qualora non vi siano già più di due farmaci di cui
 --? almeno uno basato sullo stesso principio attivo, aventi, ciascuno, un’
@@ -48,4 +26,3 @@ from (
 --? visite di accertamento, quante ne hanno effettuate per ogni visita iniziale,
 --? e il cognome del medico che ha effettuato tale visita iniziale.
 --? Gestire la materialized view mediante deferred refresh con cadenza trimestrale.
---todo: ======================================================================
