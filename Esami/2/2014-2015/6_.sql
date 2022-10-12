191d9b15fb936d9a4e0bb4a0946d7fb3edfa2a1d
Filippo <pippodelministro@gmail.com>
2022-10-12 18:10:08 +0200

    Ottobre-12_2

--- Esami/2/2014-2015/6_.sql
+++ Esami/2/2014-2015/6_.sql
@@ -54,24 +54,3 @@ from Esordio E2 natural join (	-- elimio tutti esordi che fanno join
 where E2.Patologia in (select * from PatologieMuscolari)
 	and E2.DataGuarigione is not null
 
-
-
-Negli ultimi mesi, la direzione della clinica è interessata al fenomeno della resistenza alle terapie per la pa-
-tologia influenzale. I pazienti target sono gli anziani aventi più di ottanta anni, affetti da almeno due pato-
-logie croniche. Dato un paziente target, interessano i suoi esordi di influenza degli ultimi tre anni. Conside-
-rato un esordio i , sia T il numero di terapie effettuate per curarlo, e sia d ij la durata
-� T della terapia j relativa
-all’esordio i . La resistenza della patologia nell’esordio i è espressa da: r i = T 1 j=1 d ij . Supponendo che
-gli esordi di influenza del paziente considerato siano E , se è r 1 < r 2 < · · · < r E , allora vi è una resistenza
-ai farmaci per il trattamento dell’influenza, e il tasso di resistenza è quantificabile come
-T DR = � E
-r
-2
-i=1 (r i − r)
-,
-� E
-dove r = E 1 i=1 r i . Scrivere una function per il calcolo del T DR , e il codice per il deferred full refresh
-mensile di una materialized view contenente il codice fiscale di un paziente e il relativo T DR .
-
-
-
