67be4f4b570d9483fc0919d02264f5650ed2a1ca
Filippo <pippodelministro@gmail.com>
2022-11-10 12:35:33 +0100

    Novembre-10_1

--- Esami/2/2018-2019/4.sql
+++ Esami/2/2018-2019/4.sql
@@ -1,51 +1,56 @@
---? Scrivere una query che restituisca la percentuale media di esenzione delle patologie
---? cardiache per le quali vi sono solo esordi cronici con gravita' superiore a 7
-select P.Nome, avg(P.PercEsenzione) as MediaEsenzione
-from Patologia P
-where P.SettoreMedico = 'Cardiologia' 
-	 and Nome not in (
-			select E.Patologia
-			from Esordio E
-			where E.Gravita < 7
-				or E.Cronica = 'no'
+--? Scrivere una query che restituisca le patologie gastriche che, in pazienti di età compresa fra 40 e 50 anni
+--? (entrambi compiuti), negli ultimi 20 anni, si sono manifestate una o più volte, ma solo d'estate
+select distinct E.Patologia
+from Esordio E
+where E.Paziente in (
+	select P.CodFiscale
+    from Paziente P
+    where current_date between P.DataNascita + interval 40 year and P.DataNascita + interval 50 year 
 )
-group by P.Nome
+	and E.Patologia in (
+		select PA.Nome
+		from Patologia PA
+		where PA.SettoreMedico = 'Gastroenterologia'
+)
+	and E.DataEsordio > current_date() - interval 20 year
+    and month(E.DataEsordio) between 6 and 8				-- solo d'estate
 
---? Implementare sotred procedure drug_usage_position() che riceva come par un farmaco e ne
---? restituisca la posizione in una classifica nella quale un farmaco occupa una posizione
---? tanto alta qunate pi terapie si basano su di esso. Qualora vi siano h farmaci a parimerito
---? in posizione k, il successivo occupa la posizione k+h
-drop procedure if exists drug_usage_position;
+
+--? Implementare un analytic function efficiente (tramite select statement con var user defined) che, dato un
+--? farmaco, ne restituisca il nome commerciale e il codice fiscale del primo paziente che lo ha utilizzato
+drop function if exists primo_paziente;
 delimiter $$
-create procedure drug_usage_position(
-		in _farmaco char(50),
-        out posizione_ int
+create function primo_paziente(
+	_farmaco char(50)
 )
+returns char(50) deterministic
 begin
-	create or replace view
-	RankFarmaci as (
-			select T.Farmaco, rank() over (order by count(*) desc) as Posizione
-			from Terapia T
-			group by T.Farmaco
-	);
+	declare ret char(50) default null;
     
-    set posizione_ = (
-		select RF.Posizione
-		from RankFarmaci RF
-		where RF.Farmaco = _farmaco
-	);
-    
-    drop view RankFarmaci;
+	select T.Paziente
+	from Terapia T
+	where T.Farmaco = 'Lyrica'
+	order by T.DataInizioTerapia
+	limit 1
+		into ret;
+
+	return ret;
 end $$
 delimiter ;
 
---? Ai fini di un analisi predittiva dello stress si desidera effettura un'aggregazione periodica dei
---? dati relativi agli esordi di patologie a carico dello stomaco. In tale aggregazione, ogni esordio E
---? a carico dello stomaco è associato a un valore ricavato aggregandone i dati con quelli dell'esordio
---? E_prec e successivo E_succ  dello stesso paziente, relativi alla stessa patologia. Creare una MV
---? MV_Stomaco che, per ciascun esordio E contenga il nome della patologia, il codice fiscale del
---? paziente, la data dell'esordio e il numero medio di giorni fra quelli trascorsi dall'inizio dell'
---? esordio E_prec, l'esordio E, e l'esordio E_succ.
---? Effettuare il build a partire dal primo gennaio 2010 e implementare un deferred incremental refresh
---? in modalità complete con cadenza mensile. Progettare oppurtunamente la LOG Table in modo da non effettuare
---? accessi ai raw data durante il refresh.
+
+--? Il tasso di incidenza della patologie è un informazione di vitale importanza. E' soprattutto grazie al suo
+--? monitoraggio che il MdS riesca a identificare trend di contagio, consentendo una gestione tempestiva delle
+--? criticità e una sensibilizzazione sempre più efficace, oggi agevlata dalla risonanza dei moderni social media.
+--? In un contesto come questo, si richiede di dotare il DB della clinica di una MV TASSO_INCIDENZA contentente,
+--? per patologia e città, il nome della patologia, il nome della città e il tasso di incidenza stagionale, 
+--? degli ultmi 20 anni, ciascuno in una colonna. Il tasso di incidenza stagionale di una patologia P in una
+--? città C nella stagione S si esprime come
+--? 		J = (A_p,c,s/N_c)^k
+--? 			· A_p,c,s : numero di pazienti per città C che hanno contratto patologia P in S
+--? 			· N_c     : numero di pazienti per città C
+--? 			· k = E_p,c,s/A_p,c,s se A_p,c,s != 0, altrimenti = 1
+--? 			· E_p,c,s : numero di esordi della patologia P in C in S
+--? 
+--? Implementare stored function per calcolo incidenza stagionale  			
+
