d8932e056746fecf14249d139b20dff73a8e6002
Filippo <pippodelministro@gmail.com>
2022-11-19 11:16:58 +0100

    Novembre-19_1

--- Esami/2/Orali/2021.sql
+++ Esami/2/Orali/2021.sql
@@ -4,7 +4,27 @@
 --? numero di terapie, considerando nel complesso le varie patologie, e la posologia
 --? media. In caso di pari merito fra i farmaci usati da un paziente, completare il record
 --? con valori NULL. 
+with Utilizzi as (
+	select T.Paziente, T.Farmaco, count(*) as NumUtilizzi, avg(T.Posologia) as Media
+	from Terapia T
+	group by T.Paziente, T.Farmaco
+)
 
+select U1.*
+from Utilizzi U1 inner join (
+	select U.Paziente, max(U.NumUtilizzi) as MaxUtilizzi
+	from Utilizzi U
+	group by U.Paziente
+) as D on U1.Paziente = D.Paziente
+	   and U1.NumUtilizzi = D.MaxUtilizzi
+where U1.Paziente in (		-- pazienti target
+	select distinct E.Paziente
+	from Esordio E inner join Patologia PA on E.Patologia = PA.Nome
+				   inner join Paziente P on E.Paziente = P.CodFiscale
+	where (P.Citta = 'Pisa' or P.Citta = 'Firenze')
+	group by E.Paziente, PA.SettoreMedico
+	having count(distinct E.Patologia)
+)
 
 
 --? Considerate le terapie dei pazienti aventi reddito inferiore al reddito medio della loro
@@ -13,6 +33,47 @@
 --? farmaco più economico basato sullo stesso principio attivo del farmaco
 --? effettivamente assunto, avrebbe ottenuto un risparmio sul costo totale della terapia
 --? superiore al 50%, e a quanto sarebbe ammontato tale risparmio.”
+
+with
+FarmaciCheap as (
+	select I1.Farmaco as FarmacoCheap, I1.Patologia, F1.Costo, F1.Pezzi
+	from Indicazione I1 inner join Farmaco F1 on I1.Farmaco = F1.NomeCommerciale inner join (
+		select I.Patologia, min(F.Costo) as CostoMinimo
+		from Indicazione I inner join Farmaco F on I.Farmaco = F.NomeCommerciale
+		group by I.Patologia
+	) as D on I1.Patologia = D.Patologia
+		   and F1.Costo = D.CostoMinimo
+),
+DuarataTerapia as (
+	select T.Paziente, avg(datediff(T.DataFineTerapia, T.DataInizioTerapia)) as MediaDurata
+	from Terapia T
+	where T.DataFineTerapia is not null
+	group by T.Paziente
+)
+
+
+select D.Paziente, D.Cognome, D.MediaDurata, D.CostoEff - D.CostoIp as Risparmio
+from (
+	select T.Paziente, P.Cognome, DT.MediaDurata,
+			(floor(datediff(T.DataFineTerapia, T.DataInizioTerapia) * T.Posologia / F.Pezzi) + 1) * F.Costo as CostoEff,
+			(floor(datediff(T.DataFineTerapia, T.DataInizioTerapia) * T.Posologia / FC.Pezzi) + 1) * FC.Costo as CostoIp
+	from Terapia T inner join Farmaco F on T.Farmaco = F.NomeCommerciale
+				   inner join FarmaciCheap FC on T.Patologia = FC.Patologia
+				   inner join Paziente P on T.Paziente = P.CodFiscale
+                   inner join DuarataTerapia DT on T.Paziente = DT.Paziente
+	where T.Paziente in ( -- pazienti target
+		select D.CodFiscale as Paziente
+		from (
+			select *, avg(P.Reddito) over (partition by P.Citta) as RedditoCitta
+			from Paziente P
+		) as D
+	where D.Reddito < D.RedditoCitta
+	)
+		and T.DataFineTerapia is not null
+		and T.Farmaco <> FC.FarmacoCheap
+) as D
+where D.CostoIp < 0.5 * D.CostoEff
+
 
 
 --? Implementare una stored procedure che, presa in ingresso una data d e una città di
